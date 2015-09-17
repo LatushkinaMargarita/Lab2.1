fd18598c88db7317f027abdd1e41d60247547651
LatushkinaMargarita <marlatuha@mail.ru>
2015-09-17 11:54:01 +0300

    lab2.2

---
+++ lab2.2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int power(int x, int y)
+{
+	int ff=1;
+	for (int i =1 ;i<=y;i++)
+		ff *=x;
+	return ff;
+}
+int main()
+{
+	
+	double pi=1,  e;
+	cout << "Input accuracy e"<< endl;
+	cin >> e;
+	double n=1,z;
+    int i = 0;
+	int f=1;
+     do
+    {
+		z = (2.0*i + 1.0)/(2.0*i+3.0);
+        n= n*(-1)*z  ;
+
+		pi += n;
+        i++;
+		f++;
+    }
+    while (fabs(n) > e);
+
+		
+	
+	cout << "Pi= " <<4* pi << endl;
+	system ("pause");
+	return 0;
+}
