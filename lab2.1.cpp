56b3fbf443f549538c1393a95d39cde6fa2d48fd
LatushkinaMargarita <marlatuha@mail.ru>
2015-09-17 11:50:15 +0300

    lab2.1

---
+++ lab2.1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	double x, e;
+	cout << "Input x" << endl;
+	cin >> x;
+	cout << "Input accurancy e" << endl;
+	cin >> e;
+    double n = x;
+    double sum = 0.0;
+    int i = 1;
+     do
+    {
+        sum += n;
+        n *= -1.0 * x * x / ((2 * i) * (2 * i + 1));
+        i++;
+    }
+    while (fabs(n) > e);
+	cout << sum << endl;
+	system("pause");
+	return 0;
+
+}
