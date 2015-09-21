bae8e5b5c81f77e51e609cdf32d47001f3cc2687
kubitskivlad <kubitskivladis@gmail.com>
2015-09-21 19:44:12 +0300

    Update lab2.1.cpp

--- lab2.1.cpp
+++ lab2.1.cpp
@@ -1,24 +1,41 @@
 #include <iostream>
+#include <math.h>
+#include <stdio.h>
 using namespace std;
-int main ()
+double osnova (int X1, double E1)
 {
-	double x, e;
-	cout << "Input x" << endl;
-	cin >> x;
-	cout << "Input accurancy e" << endl;
-	cin >> e;
-    double n = x;
+    double n = X1;
     double sum = 0.0;
     int i = 1;
-     do
+    do
     {
         sum += n;
-        n *= -1.0 * x * x / ((2 * i) * (2 * i + 1));
+        n *= -1.0 * X1 * X1 / ((2 * i) * (2 * i + 1));
         i++;
     }
-    while (fabs(n) > e);
-	cout << sum << endl;
-	system("pause");
-	return 0;
+    while (fabs(n) > E1);
+    
+    return sum;
+}
 
+void assert (int X, double E)
+{
+    if (fabs (sin(X)- osnova( X, E)) < E)
+    {
+        cout<<" ok "<<endl;
+    }
+    else
+    {
+        cout<<" ne ok "<<X<<endl;
+    }
+}
+int main ()
+{
+    assert(10,0.01);
+     assert(8,0.00001);
+     assert(7,0.01);
+     assert(9,0.0001);
+     assert(5,0.0001);
+    return 0;
+    
 }
