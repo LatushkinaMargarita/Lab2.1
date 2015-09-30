311512866ae9b9ba3d27066a2a585f9c0c567c84
LatushkinaMargarita <marlatuha@mail.ru>
2015-09-30 22:39:11 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -15,10 +15,29 @@ void main()
 {
 	int sum[10000] = { 0 };
 	int pr[10000] = { 0 };
-
-    int a[2] = { 3, 4 };
+	int rezsum[10000] = { 4, -1 };
+	int rezpr[10000] = { 3, -11, 20 };
+	int a[2] = { 3, 4 };
 	int b[2] = { 1, -5 };
-	zapysk(2, a, b, sum, pr);
-	for (int i = 0; i < 2; i++)
-		cout << sum[i] << "  " << pr[i] << endl;
+	if (sum[1]==rezsum[1])
+		cout<< "ok"<<endl;
+	else 
+		cout<< "false"<<endl;
+	if (sum[2]==rezsum[2])
+		cout<< "ok"<<endl;
+	else 
+		cout<< "false"<<endl;
+	if (pr[1]==rezpr[1])
+		cout<< "ok"<<endl;
+	else 
+		cout<< "false"<<endl;
+	if (pr[2]==rezpr[2])
+		cout<< "ok"<<endl;
+	else 
+		cout<< "false"<<endl;
+	if (pr[3]==rezpr[3])
+		cout<< "ok"<<endl;
+	else 
+		cout<< "false"<<endl;
+	system("pause");
 }
