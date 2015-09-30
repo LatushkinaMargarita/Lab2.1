14fc35f3ff1e6b5d57f989554306e5b15833b7f3
LatushkinaMargarita <marlatuha@mail.ru>
2015-09-30 22:48:23 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-using namespace std;
 
 void zapysk(int kol, int a[], int b[], int sum[], int pr[])
 {
@@ -20,23 +18,11 @@ void main()
 	int a[2] = { 3, 4 };
 	int b[2] = { 1, -5 };
 	zapysk(2,a,b,sum,pr);
-	if (sum[0]==rezsum[0])
+	if (sum[0]==rezsum[0] && sum[1]==rezsum[1])
 		cout<< "ok"<<endl;
 	else 
 		cout<< "false"<<endl;
-	if (sum[1]==rezsum[1])
-		cout<< "ok"<<endl;
-	else 
-		cout<< "false"<<endl;
-	if (pr[0]==rezpr[0])
-		cout<< "ok"<<endl;
-	else 
-		cout<< "false"<<endl;
-	if (pr[1]==rezpr[1])
-		cout<< "ok"<<endl;
-	else 
-		cout<< "false"<<endl;
-	if (pr[2]==rezpr[2])
+	if (pr[0]==rezpr[0]&& pr[1]==rezpr[1] && pr[2]==rezpr[2])
 		cout<< "ok"<<endl;
 	else 
 		cout<< "false"<<endl;
