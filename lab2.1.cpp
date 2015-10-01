bed74415c46ca29c210bb50559f5dc5d6ca0f428
Gleb-Vinnikov <gleb-vinnikov@mail.ru>
2015-10-01 12:03:17 +0300

    BUG FIX LAB 3_1

--- lab2.1.cpp
+++ lab2.1.cpp
@@ -1,41 +1,24 @@
 #include <iostream>
-#include <math.h>
-#include <stdio.h>
 using namespace std;
-double osnova (int X1, double E1)
+
+void zapysk(int kol, int a[], int b[], int sum[], int pr[])
 {
-    double n = X1;
-    double sum = 0.0;
-    int i = 1;
-    do
-    {
-        sum += n;
-        n *= -1.0 * X1 * X1 / ((2 * i) * (2 * i + 1));
-        i++;
-    }
-    while (fabs(n) > E1);
-    
-    return sum;
+	for (int i = 0; i < kol; i++)
+		sum[i] = a[i] + b[i];
+
+	for (int i = 0; i < kol; i++)
+	for (int j = 0; j < kol; j++)
+		pr[i] = pr[i] + a[i] * b[j];
 }
 
-void assert (int X, double E)
+void main()
 {
-    if (fabs (sin(X)- osnova( X, E)) < E)
-    {
-        cout<<" ok "<<endl;
-    }
-    else
-    {
-        cout<<" ne ok "<<X<<endl;
-    }
-}
-int main ()
-{
-    assert(10,0.01);
-     assert(8,0.00001);
-     assert(7,0.01);
-     assert(9,0.0001);
-     assert(5,0.0001);
-    return 0;
-    
+	int sum[10000] = { 0 };
+	int pr[10000] = { 0 };
+
+	int a[2] = { 3, 4 };
+	int b[2] = { 1, -5 };
+	zapysk(2, a, b, sum, pr);
+	for (int i = 0; i < 2; i++)
+		cout << sum[i] << "  " << pr[i] << endl;
 }
