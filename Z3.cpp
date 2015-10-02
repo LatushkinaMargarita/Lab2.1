5ad5dec0e608647be9bd8eaf3703a09ad4e880c9
Gleb-Vinnikov <gleb-vinnikov@mail.ru>
2015-10-02 18:33:06 +0300

    UPDATE Z3

--- Z3.cpp
+++ Z3.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
 
-void zapysk(int kol, int kol1, int a[], int b[], int sum[], int pr[])
+void zapysk(int kol, int kol1, int a[], int b[], int sum[], int pr[], int ch[])
 {
 	if (kol > kol1)
 	{
+		ch[0] = kol;
 		for (int i = 0; i < kol; i++)
 			sum[i] = a[i];
 
@@ -17,6 +18,7 @@ void zapysk(int kol, int kol1, int a[], int b[], int sum[], int pr[])
 	}
 	else
 	{
+		ch[0] = kol1;
 		for (int i = 0; i < kol1; i++)
 			sum[i] = b[i];
 
@@ -29,7 +31,7 @@ void zapysk(int kol, int kol1, int a[], int b[], int sum[], int pr[])
 	}
 
 
-
+	ch[1] = kol + kol1 - 1;
 	for (int i = 0; i < kol; i++)
 	for (int j = 0; j < kol1; j++)
 		pr[i + j] = pr[i + j] + a[i] * b[j];
@@ -37,13 +39,20 @@ void zapysk(int kol, int kol1, int a[], int b[], int sum[], int pr[])
 
 void main()
 {
+	int y;
+	int ch[2] = { 0 };
 	int sum[10000] = { 0 };
 	int pr[10000] = { 0 };
 
 	int a[2] = { 2, 1 };
 	int b[3] = { 3, 5, 8 };
-	zapysk(2, 3, a, b, sum, pr);
+	zapysk(2, 3, a, b, sum, pr, ch);
 
-	for (int i = 0; i < 5; i++)
-		cout << sum[i] << "  " << pr[i] << endl;
+	for (int i = 0; i < ch[0]; i++)
+		cout << sum[i] << "  ";
+	cout << endl;
+
+	for (int i = 0; i < ch[1]; i++)
+		cout << pr[i] << "  ";
+	cout << endl;
 }
