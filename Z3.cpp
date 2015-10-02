1d30d612e1b9b1c40a710c6bacbcae2ca3889598
Gleb-Vinnikov <gleb-vinnikov@mail.ru>
2015-10-02 17:51:03 +0300

    Update Z3

--- Z3.cpp
+++ Z3.cpp
@@ -3,17 +3,36 @@ using namespace std;
 
 void zapysk(int kol, int kol1, int a[], int b[], int sum[], int pr[])
 {
-	if (kol>kol1)
-	  for (int i = 0; i < kol; i++)
-		sum[i] = a[i] + b[i];
-	else
-	for (int i = 0; i < kol1; i++)
-		sum[i] = a[i] + b[i];
+	if (kol > kol1)
+	{
+		for (int i = 0; i < kol; i++)
+			sum[i] = a[i];
 
-	
+		int uk = kol - 1;
+		for (int i = kol1 - 1; i >= 0; i--)
+		{
+			sum[uk] = sum[uk] + b[i];
+			uk--;
+		}
+	}
+	else
+	{
+		for (int i = 0; i < kol1; i++)
+			sum[i] = b[i];
+
+		int uk = kol1 - 1;
+		for (int i = kol - 1; i >= 0; i--)
+		{
+			sum[uk] = sum[uk] + a[i];
+			uk--;
+		}
+	}
+
+
+
 	for (int i = 0; i < kol; i++)
 	for (int j = 0; j < kol1; j++)
-		pr[i+j] = pr[i+j] + a[i] * b[j];
+		pr[i + j] = pr[i + j] + a[i] * b[j];
 }
 
 void main()
@@ -21,9 +40,10 @@ void main()
 	int sum[10000] = { 0 };
 	int pr[10000] = { 0 };
 
-	int a[3] = { 3, 4, 6 };
-	int b[3] = { 1, 1, 4 };
-	zapysk(3, 3, a, b, sum, pr);
+	int a[2] = { 2, 1 };
+	int b[3] = { 3, 5, 8 };
+	zapysk(2, 3, a, b, sum, pr);
+
 	for (int i = 0; i < 5; i++)
 		cout << sum[i] << "  " << pr[i] << endl;
 }
