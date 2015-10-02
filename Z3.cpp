6d0a94f6281740aaf0a10ec25e51a2a3a298415c
LatushkinaMargarita <marlatuha@mail.ru>
2015-10-02 19:14:11 +0300

    Update Z3.cpp

--- Z3.cpp
+++ Z3.cpp
@@ -37,22 +37,57 @@ void zapysk(int kol, int kol1, int a[], int b[], int sum[], int pr[], int ch[])
 		pr[i + j] = pr[i + j] + a[i] * b[j];
 }
 
-void main()
+void assert(int sum[], int pr[], int sum_[] ,int pr_[],int ch[])
 {
-	int y;
+		
+	int k = 0;
+    for (int j = 0; j < ch[0]; j++)
+	{
+        if(sum[j]==sum_[j])
+			k++;
+	}
+    for (int i = 0; i < ch[1]; i++)
+	{
+        if(pr[i]==pr_[i])
+			k++;
+	}
+    if (k==ch[0]+ch[1])
+        cout << "ok"<< endl;
+    else
+        cout << "fail"<<endl;
+}
+int main()
+{
 	int ch[2] = { 0 };
 	int sum[10000] = { 0 };
 	int pr[10000] = { 0 };
+	int a[3] = { 3, -2, 1 };
+	int b[2] = { 5,-3};
+	zapysk(3, 2, a, b, sum, pr,ch);
+	int sum1[3]= { 3,3,-2};
+	int pr1[4] = { 15,-19,11,-3};
+	assert(sum,pr,sum1,pr1,ch);
 
-	int a[2] = { 2, 1 };
-	int b[3] = { 3, 5, 8 };
-	zapysk(2, 3, a, b, sum, pr, ch);
+	int ch1[2] = { 0 };
+	int summ[10000] = { 0 };
+	int prr[10000] = { 0 };
+	int a2[2] = { 1, -1 };
+	int b2[2] = { 1, 1 };
+	zapysk(2, 2, a2, b2, summ, prr,ch1);
+	int sum2[2]= {2,0};
+	int pr2[3] = { 1,0,-1};
+	assert(summ,prr,sum2,pr2,ch1);
+	
 
-	for (int i = 0; i < ch[0]; i++)
-		cout << sum[i] << "  ";
-	cout << endl;
-
-	for (int i = 0; i < ch[1]; i++)
-		cout << pr[i] << "  ";
-	cout << endl;
+	int ch2[2] = { 0 };
+	int summ2[10000] = { 0 };
+	int prr2[10000] = { 0 };
+	int a3[2] = { 2, -5 };
+	int b3[2] = { 3, -8 };
+	zapysk(2, 2, a3, b3, summ2, prr2,ch2);
+	int sum3[2]= {5,-13};
+	int pr3[3] = { 6,-31,40};
+	assert(summ2,prr2,sum3,pr3,ch2);
+	system("pause");
+	return 0;
 }
